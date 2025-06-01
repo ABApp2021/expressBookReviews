55543f47a95bc5ee62e87c5d7b653f0d32fd08b7
ABApp2021 <bwatunyu@gmail.com>
2025-06-01 10:44:07 -0400

    Register

--- final_project/index.js
+++ final_project/index.js
@@ -3,11 +3,10 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
+const { isValid } = require('./router/auth_users.js');
 const app = express();
 
 app.use(express.json());
-
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
