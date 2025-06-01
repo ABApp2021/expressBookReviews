277cb1f32aa02c515fab2790120b883a7b37d285
ABApp2021 <bwatunyu@gmail.com>
2025-06-01 09:45:41 -0400

    initial commit

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,12 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if (req.session && req.session.authorization) {
+    // Assuming session contains an object { username: "user" }
+    next(); // User is authenticated
+} else {
+    return res.status(403).json({ message: "User not authenticated" });
+}
 });
  
 const PORT =5000;
